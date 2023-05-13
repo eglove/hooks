a0c0eaf68eae339354045e8ab36c187d3a5f1013
Ethan Glover <hello@ethang.email>
2023-05-13 12:46:35 -0500

    Update build script and dependencies in package.json and add tsconfig.json.

--- build.mjs
+++ build.mjs
@@ -2,13 +2,14 @@ import { rimraf } from 'rimraf';
 import { build as esbuild } from 'esbuild';
 import { globPlugin } from 'esbuild-plugin-glob';
 import fs from 'node:fs';
+import { execSync } from 'child_process';
 
 await rimraf('dist');
 
 await esbuild({
   bundle: false,
   entryPoints: ['src/*.ts'],
-  loader: {'.ts': 'ts'},
+  loader: { '.ts': 'ts' },
   minify: true,
   outdir: 'dist',
   platform: 'browser',
