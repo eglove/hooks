5bfdf35ec1697fb247748a61b4aa64d131a6caee
Ethan Glover <hello@ethang.email>
2023-05-12 20:53:58 -0500

    Update package.json to use build.mjs instead of build.ts for build script.

---
+++ build.mjs
@@ -0,0 +1,25 @@
+import { rimraf } from 'rimraf';
+import { build as esbuild } from 'esbuild';
+import { globPlugin } from 'esbuild-plugin-glob';
+import fs from 'node:fs';
+
+await rimraf('dist');
+
+await esbuild({
+  bundle: false,
+  entryPoints: ['src/*.ts'],
+  loader: {'.ts': 'ts'},
+  minify: true,
+  outdir: 'dist',
+  platform: 'browser',
+  plugins: [globPlugin()],
+  sourcemap: true,
+  target: 'esnext',
+});
+
+fs.copyFileSync(
+  'package.json',
+  'dist/package.json',
+);
+
+execSync('cd dist && npm publish --access public && cd ..');
