e415dd0707f2b09031498e4807d6037b9106a679
Ethan Glover <hello@ethang.email>
2023-10-08 17:59:57 -0500

    Add 'useSet' React hook for set operations.
    A new custom hook called 'useSet' was added. This hook utilizes React's useState to encapsulate set operations such as 'add' and 'remove' within a stateful variable. It returns an object with 'add', 'remove', and 'set' methods to manipulate the set. This implementation provides a simpler and more readable solution to managing sets in functional components.

---
+++ src/use-set.ts
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+type UseSet<SetType> = Omit<Set<SetType>, 'add' | 'delete'>;
+
+type UseSetReturn<SetType> = {
+  add: (value: SetType) => void;
+  remove: (value: SetType) => void;
+  set: UseSet<SetType>;
+};
+
+export function useSet<SetType>(
+  values: SetType[] | undefined | null,
+): UseSetReturn<SetType> {
+  const [set, setSet] = useState(new Set(values));
+
+  const add = (value: SetType): void => {
+    setSet(set_ => {
+      return set_.add(value);
+    });
+  };
+
+  const remove = (value: SetType): void => {
+    setSet(set_ => {
+      set_.delete(value);
+      return new Set(set_);
+    });
+  };
+
+  return { add, remove, set };
+}
