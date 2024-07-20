fe855e5e6b71160277a8ec90f73f4570bb02aed3
Ethan Glover <hello@ethang.email>
2024-07-20 15:08:50 -0500

    useasync

---
+++ src/use-async.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function useAsync<T, E>(callback: () => Promise<T>) {
+  const [result, setResult] = useState<T>();
+  const [error, setError] = useState<E>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    callback()
+      .then((result_) => {
+        setResult(result_);
+      })
+      .catch((error_: unknown) => {
+        setError(error_ as E);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return { error, isLoading, result };
+}
